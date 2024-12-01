ee815171ccbb7b67ebe3f8f077e05bd90a770f4e
Tamanna Mondal <tamannamondal0111@gmail.com>
2024-12-01 16:27:24 +0530

    Fast Tash : Amazon Product display

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import Amazon_box from './Component/Amazon_box'
+import AmazonImage from './Component/AmazonImage'
+import AmazonMoney from './Component/AmazonMoney'
+function App() {
+
+
+  return (
+    <>
+    <div className='main'>
+      
+      <AmazonImage/>   <Amazon_box/> <AmazonMoney/>
+      
+    </div>
+   <hr/>
+   <div className='main'>
+      
+      <AmazonImage/>   <Amazon_box/> <AmazonMoney/>
+      
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/AmazonImage.jsx
@@ -0,0 +1,12 @@
+import './AmazonImage.css'
+export default function AmazonImage(){
+    return(
+        <>
+           <div className="image">
+           <img  src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-14-pro-family-select?wid=940&hei=1112&fmt=png-alpha&.v=1678403206946"
+                    alt="iPhone" />
+          
+           </div>
+        </>
+    )
+}
